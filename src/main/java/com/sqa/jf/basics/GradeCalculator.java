acd148cc71e9d190efdb628071fccc7232f2fd62
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-01-23 20:30:42 -0800

    Add Commented Structure for GradeCalculator Class Exercise

---
+++ src/main/java/com/sqa/jf/basics/GradeCalculator.java
@@ -0,0 +1,44 @@
+/**
+ * File Name: GradeCalculator.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Jan 23, 2016
+ */
+package com.sqa.jf.basics;
+
+/**
+ * GradeCalculator //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class GradeCalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Declare Variables
+		// Declare String input variable
+		// Declare int numGrades variable
+		// Declare double totalGradeValue and assign to 0
+		// Declare double finalGradeAverage
+		// Welcome User to application
+		// Create Scanner object and assign to variable scanner
+		// Ask the user how many grades they have
+		// Assign the input variable to the next line from the scanner object
+		// Convert the input to numGrades variable
+		// Use a for loop (initialization; boolean condition; increment) {
+		// Ask user what grade num i is
+		// Capture the user's grade inside the String variable input
+		// Convert the input to an int variable
+		// Calculate final grade
+		// Tell user the final grade
+	}
+}
