e5e9d5b3468d1465d3f782f86993c8caab2cde49
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-01-23 20:29:43 -0800

    Add Demo Notes for Count100 Class Exercise

---
+++ src/main/java/com/sqa/jf/basics/Count100.java
@@ -0,0 +1,32 @@
+/**
+ * File Name: Count100.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Jan 23, 2016
+ */
+package com.sqa.jf.basics;
+
+/**
+ * Count100 //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Count100 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*** Demo Code **/
+		for (int i = 0; i < 10; i++) {
+			System.out.println("i is " + i);
+		}
+	}
+}
